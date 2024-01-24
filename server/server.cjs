c3818ac87a2202069003ea97a7e68c808ea7f89c
Prince Lawrence Jacinto <jacinto.princelawrence@gmail.com>
2024-01-24 21:49:20 +0800

    fixes vulnerability issues/connected to db

--- server/server.cjs
+++ server/server.cjs
@@ -1,7 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3001;
+const uri = 'mongodb+srv://Lyfie:pass123@dbsas.mtpeotb.mongodb.net/';
+
+//db connection >>
+mongoose.connect(uri)
+    .then(() => {
+        console.log("Connected to db");
+    }).catch(() => {
+        console.log("error putang ina");
+    });
 
 app.use(cors());
 app.use(express.json()); // Parse JSON data from the request body
