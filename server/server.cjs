e89e026e6322a9019241b82a838fa0b3b171d947
Prince Lawrence Jacinto <jacinto.princelawrence@gmail.com>
2024-02-18 11:00:36 +0800

    fixes duplicate email errs

--- server/server.cjs
+++ server/server.cjs
@@ -76,7 +76,12 @@ app.post('/api/register', async (req, res) => {
   }
 
   const { email, pass, role } = req.body;
+  const emailExist = await User.findOne({email: email})
     try {
+
+      if (email == emailExist.email){ 
+        res.send("User Exists!")
+      } else {
         const verificationToken = generateVerificationToken()
         const newUser = new User({
           email: email,
@@ -97,6 +102,7 @@ app.post('/api/register', async (req, res) => {
           role: role,
           verificationToken: verificationToken
         });
+      }
       } catch (error) {
         console.error('Error saving user:', error);
         res.status(500).json({ error: 'Server error' });
