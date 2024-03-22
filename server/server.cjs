35b55f36df57abba59ec1d0ef98a194bd6aeb8df
Prince Lawrence Jacinto <jacinto.princelawrence@gmail.com>
2024-03-22 13:04:05 +0800

    added fetch shelter and fix log in local users

--- server/server.cjs
+++ server/server.cjs
@@ -354,7 +354,8 @@ app.post("/api/login", async (req, res) => {
                   status: 200,
                   message: "Log in Success",
                   checked: true,
-                  user: user._id
+                  user: user._id,
+                  role
               });
           } else {
               // Passwords don't match, send failure response
@@ -455,6 +456,19 @@ app.get("/getPet", async (req, res) => {
     res.send({
       status: 200,
       allPets,
+    });
+  } catch (err) {
+    console.log("error: ", err);
+  }
+});
+
+app.get("/getShelter", async (req, res) => {
+  try {
+    const allShelter = await User.find({role: 'Rescue Shelter'});
+    res.send({
+      status: 200,
+      allShelter,
+      message: "use allShelter._id to get ID"
     });
   } catch (err) {
     console.log("error: ", err);
